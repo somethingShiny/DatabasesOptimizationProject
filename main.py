6af4a8c17dc63786cd0372c45d7b68b0b91ff324
Aaron Smittle <aaronSmittle@gmail.com>
2014-11-18 10:46:07 -0600

    Added shell for main and read input scripts

---
+++ main.py
@@ -0,0 +1,12 @@
+#required imports
+from read_input import get_query
+
+
+def main():
+    #Do stuff
+    get_query()
+    exit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ read_input.py
@@ -0,0 +1,7 @@
+#required imports (probably codecs for utf-8 encoding and either stdin or file)
+
+
+def get_query():
+    #READ QUERY (EITHER FROM A FILE OR FROM STDIN)
+    #RETURN QUERY IN SOME FORMAT (STRING, OBJECT, ARRAY)
+    return
