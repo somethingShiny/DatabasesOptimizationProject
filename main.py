fdf517361a7f7b9c23f99061e877ff5371a8b505
Aaron Smittle <aaronSmittle@gmail.com>
2014-11-20 17:16:06 -0600

    Added reading input from stdin Added splitting queries into nested lists

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 #required imports
+# -*- coding: utf-8 -*-
 from read_input import get_query
+from read_input import parse_query
 from convert_query import convert
+from sys import stdout
 
-PROJECT = 'π'.encode('utf-8')
-SELECT = '(sigma)'
+PROJECT = 'π'.decode('utf-8')
+SELECT = 'σ'.decode('utf-8')
 
 
 def main():
-    #Do stuff
+    print '\n\n'
     query = get_query()
-    print(query)
+    for q in query:
+        for l in q:
+            stdout.write(l + '\n')
+        print '\n'
+    print '\n\n'
     convert(query)
     exit()
 
--- read_input.py
+++ read_input.py
@@ -1,7 +1,62 @@
-#required imports (probably codecs for utf-8 encoding and either stdin or file)
+# required imports (probably codecs for utf-8 encoding and either stdin or file)
+# -*- coding: utf-8 -*-
 from sys import stdin
+import re
 
 
 def get_query():
+    new_query = re.compile(r'[a-z]\. ')
+    nested_query = re.compile(r'(.*)')
     query = stdin.readlines()
-    return query
+
+    return parse_query(query)
+    lines = []
+    queries = []
+    for line in query:
+        line = line.strip('\n')
+        if (re.match(new_query, line)):
+            add = True
+        line = re.sub(new_query, '', line)
+
+        if add:
+            queries.append(lines)
+            lines = []
+            add = False
+        lines.append(line)
+
+    grouped_queries = []
+    for i in range(1, len(queries)):
+        grouped_queries.append(queries[i])
+
+    return grouped_queries
+
+
+def parse_query(query):
+    add = False
+    new_query = re.compile(r'[a-z]\. ')
+    lines = []
+    queries = []
+
+    for line in query:
+        line = line.replace(')', '')  #Strip off closing parenthesis from nested queries
+        line = line.strip('\n')  #Strip newlines
+        if re.match(new_query, line):
+            add = True
+        line = re.sub(new_query, '', line)
+        if add:
+            queries.append(lines)
+            lines = []
+            add = False
+        split_lines = line.split('(')
+        if (len(split_lines) > 1):
+            for line in split_lines:
+                if len(line.strip(' ')):
+                    lines.append(line)
+        else:
+            lines.append(line)
+    queries.append(lines)
+    grouped_queries = []
+    for i in range(1, len(queries)):
+        grouped_queries.append(queries[i])
+
+    return grouped_queries
